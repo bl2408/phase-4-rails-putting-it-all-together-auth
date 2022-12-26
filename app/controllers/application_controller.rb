7e32ea2f25595059a5a21f3b160e017c1dd22a84
Brian Lambert <brianlambert2408@gmail.com>
2022-12-26 13:44:53 +1100

    Completed tasks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  before_action :authorize
+  rescue_from ActiveRecord::RecordInvalid, with: :show_errors
+
+  private
+
+
+  def authorize
+    user = User.find_by(id: session[:user_id])
+    return render json: { errors: ["Not authorized!"] }, status: :unauthorized unless user
+  end
+
+  def show_errors e
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
 end
